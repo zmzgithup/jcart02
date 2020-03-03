454467183cf3c5d7fe21da6cf7cd7b354e9fc4ac
zmz <zmz20000502@163.com>
2020-03-03 15:12:51 +0800

    add administrator index

---
+++ jcart-administration-front/viewmodels/administrator-index.js
@@ -0,0 +1,81 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        pageInfo:'',
+        pageNum:'',
+        selectedAdministrators:[]
+    },
+    computed: {
+        selectedAdministratorIds() {
+            return this.selectedAdministrators.map(a => a.administratorId);
+        }
+    },
+    mounted(){
+        console.log('view mounted')
+        this.getAdministrators();
+    },
+    methods: {
+        handlePageChange(val) {
+            console.log('page change', val);
+            this.pageNum = val;
+            this.getAdministrators();
+        },
+        handleDelete(index, row) {
+            console.log('delete click');
+
+            if (confirm("确认删除？")) {
+                this.deleteAdministrator(row.administratorId);
+            }
+        },
+        handleBatchDeleteClick() {
+            console.log('batch delete click');
+            if (confirm("确认删除？")) {
+                this.batchDeleteAdministrators();
+            }
+        },
+        handleSelectionChange(val) {
+            console.log('selection change', val);
+            this.selectedAdministrators = val;
+        },
+        deleteAdministrator(administratorId) {
+            axios.post('/administrator/delete', administratorId, {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    alert('删除成功');
+                    location.reload();
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+        batchDeleteAdministrators() {
+            axios.post('/administrator/batchDelete', this.selectedAdministratorIds)
+                .then(function (response) {
+                    console.log(response);
+                    alert('批删成功');
+                    location.reload();
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+        getAdministrators() {
+            axios.get('/administrator/getList', {
+                params: {
+                    pageNum: this.pageNum
+                }
+            })
+                .then(function (response) {
+                    console.log(response);
+                    app.pageInfo = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+    }
+})
