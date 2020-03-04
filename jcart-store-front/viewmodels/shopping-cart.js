0c8147bd006dfc103adecc6f3fbd2a4e4f37aee8
zmz <zmz20000502@163.com>
2020-03-04 13:59:20 +0800

    fix store shopping cart page total price

--- jcart-store-front/viewmodels/shopping-cart.js
+++ jcart-store-front/viewmodels/shopping-cart.js
@@ -13,6 +13,12 @@ var app = new Vue({
             console.log('delete click');
             this.myShoppingCart.splice(index,1);
             localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
+            this.$message.success('修改购物车成功')
+        },
+        handleUpdate(){
+            console.log('update click');
+            localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
+            this.$message.success('删除购物车成功')
         }
     }
 })
