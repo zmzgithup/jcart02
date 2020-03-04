e564f0eddac08cace018f699b07fdcee5e1adcf3
zmz <zmz20000502@163.com>
2020-03-04 13:50:18 +0800

    add store shopping cart delete

--- jcart-store-front/viewmodels/shopping-cart.js
+++ jcart-store-front/viewmodels/shopping-cart.js
@@ -7,5 +7,12 @@ var app = new Vue({
         console.log('view mounted');
         var myShoppingCartJson = localStorage['myShoppingCartJson'];
         this.myShoppingCart = JSON.parse(myShoppingCartJson);
+    },
+    methods:{
+        handleDelete(index,row){
+            console.log('delete click');
+            this.myShoppingCart.splice(index,1);
+            localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
+        }
     }
 })
