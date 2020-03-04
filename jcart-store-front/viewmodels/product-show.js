81ba07c12cc95c75982190cdec965c156458d69b
zmz <zmz20000502@163.com>
2020-03-04 14:08:09 +0800

    fix store shopping cart

--- jcart-store-front/viewmodels/product-show.js
+++ jcart-store-front/viewmodels/product-show.js
@@ -39,16 +39,26 @@ var app = new Vue({
     methods:{
         handleAddToCartClick(){
             console.log('add to cart click');
-            var newProduct = {
-                productId:this.productId,
-                productCode: this.productCode,
-                productName: this.productName,
-                mainPicUrl: this.mainPicUrl,
-                unitPrice: this.price,
-                quantity: this.quantity
-            };
-            newProduct.totalPrice = this.price * this,quantity;
-            this.myShoppingCart.push(newProduct);
+            var myShoppingCartJson = localStorage['myShoppingCartJson'];
+            this.myShoppingCart = myShoppingCartJson ? JSON.parse(myShoppingCartJson) : [];
+
+            var cartProduct = this.myShoppingCart.find(p => p.productId === this.productId);
+            if (cartProduct) {
+                console.log('cart product exist');
+                var originQuantity = parseInt(cartProduct.quantity);
+                var addQuantity = parseInt(this.quantity);
+                cartProduct.quantity = originQuantity + addQuantity;
+            } else {
+                cartProduct = {
+                    productId: this.productId,
+                    productCode: this.productCode,
+                    productName: this.productName,
+                    mainPicUrl: this.mainPicUrl,
+                    unitPrice: this.price,
+                    quantity: this.quantity
+                };
+                this.myShoppingCart.push(cartProduct);
+            }
             localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
             this.$message.success('添加购物车成功');
         },
--- jcart-store-front/viewmodels/shopping-cart.js
+++ jcart-store-front/viewmodels/shopping-cart.js
@@ -6,19 +6,19 @@ var app = new Vue({
     mounted(){
         console.log('view mounted');
         var myShoppingCartJson = localStorage['myShoppingCartJson'];
-        this.myShoppingCart = JSON.parse(myShoppingCartJson);
+        this.myShoppingCart = myShoppingCartJson ? JSON.parse(myShoppingCartJson) : [];
     },
     methods:{
         handleDelete(index,row){
             console.log('delete click');
             this.myShoppingCart.splice(index,1);
             localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
-            this.$message.success('修改购物车成功')
+            this.$message.success('删除购物车成功')
         },
         handleUpdate(){
             console.log('update click');
             localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
-            this.$message.success('删除购物车成功')
+            this.$message.success('修改购物车成功')
         }
     }
 })
