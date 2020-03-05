f66cbe6b099de22f15ba4678c3a139e8f29ff361
zmz <zmz20000502@163.com>
2020-03-05 13:50:01 +0800

    add store checkout

--- jcart-store-front/viewmodels/shopping-cart.js
+++ jcart-store-front/viewmodels/shopping-cart.js
@@ -1,24 +1,42 @@
 var app = new Vue({
     el: '#app',
     data: {
-        myShoppingCart:[]
+        myShoppingCart: []
     },
-    mounted(){
+    computed: {
+        totalPrice() {
+            var subTotalPrices = this.myShoppingCart.map(p => {
+                return p.unitPrice * p.discount * p.quantity;
+            });
+            var totalPrice = subTotalPrices.reduce((a, b) => a + b, 0);
+            var totalPriceStr = totalPrice.toFixed(2);
+            totalPrice = parseFloat(totalPriceStr);
+            return totalPrice;
+        }
+    },
+    mounted() {
         console.log('view mounted');
         var myShoppingCartJson = localStorage['myShoppingCartJson'];
         this.myShoppingCart = myShoppingCartJson ? JSON.parse(myShoppingCartJson) : [];
     },
-    methods:{
-        handleDelete(index,row){
+    methods: {
+        handleDelete(index, row) {
             console.log('delete click');
-            this.myShoppingCart.splice(index,1);
-            localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
-            this.$message.success('删除购物车成功')
+            if (confirm('确定删除？')) {
+                this.myShoppingCart.splice(index, 1);
+                localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
+                this.$message.success('删除购物车成功');
+            }
         },
-        handleUpdate(){
+        handleUpdate() {
             console.log('update click');
             localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
             this.$message.success('修改购物车成功')
+        },
+        handleClearCart() {
+            console.log('clear cart click');
+            this.myShoppingCart = [];
+            localStorage.removeItem('myShoppingCartJson');
         }
     }
 })
