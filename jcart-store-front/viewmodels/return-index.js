860440706303b4ca6bd8b351d70826de05f0c087
zmz <zmz20000502@163.com>
2020-03-07 11:04:24 +0800

    add store return index page

---
+++ jcart-store-front/viewmodels/return-index.js
@@ -0,0 +1,34 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        pageInfo:'',
+        pageNum:1
+    },
+    mounted(){
+        console.log('view mounted');
+        
+        this.searchReturn();
+    },
+    methods:{
+        handlePageChange(val){
+            console.log('page change click',val);
+            this.pageNum = val;
+            this.searchReturn();
+        },
+        searchReturn(){
+            axios.get('return/getList',{
+                params:{
+                    pageNum:pagNum
+                }
+            })
+            .then(function(response){
+                console.log(response);
+                app.pageInfo = response.data;
+            })
+            .catch(function(error){
+                console.log(error);
+                
+            })
+        }
+    }
+})
