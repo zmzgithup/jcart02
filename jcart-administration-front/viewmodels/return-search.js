702fcf879bc5648f20f59958f9a39e525ace7f58
zmz <zmz20000502@163.com>
2020-03-08 10:18:29 +0800

    add administrator return search page

---
+++ jcart-administration-front/viewmodels/return-search.js
@@ -0,0 +1,34 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        pageInfo: '',
+        pageNum: 1
+    },
+    mounted() {
+        console.log('view moubted');
+
+        this.searchReturn();
+    },
+    methods: {
+        handlePageChange(val){
+            console.log('page change click',val);
+            this.pageNum = val;
+            this.searchReturn();
+        },
+        searchReturn(){
+            axios.get('/return/search',{
+                params:{
+                    pageNum : this.pageNum
+                }
+            })
+            .then(function(response){
+                console.log(response);
+                app.pageInfo = response.data;
+            })
+            .catch(function(error){
+                console.log(error);
+                
+            })
+        }
+    }
+})
