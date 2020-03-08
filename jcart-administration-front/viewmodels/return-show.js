ccdad6f38ce94155acd30ed503ffaf01ea1bbb65
zmz <zmz20000502@163.com>
2020-03-08 10:27:25 +0800

    add administrator return show page

---
+++ jcart-administration-front/viewmodels/return-show.js
@@ -0,0 +1,92 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        returnId: '',
+        orderId: '',
+        orderTimestamp: '',
+        customerId: '',
+        customerName: '',
+        mobile: '',
+        email: '',
+        status: '',
+        action: '',
+        productCode: '',
+        productName: '',
+        quantity: '',
+        reason: '',
+        opened: '',
+        comment: '',
+        createTimestamp: '',
+        updateTimestamp: '',
+        actions: [
+            { value: 0, label: '退货' },
+            { value: 1, label: '换货' },
+            { value: 2, label: '修理' }
+        ],
+        selectedAction: ''
+    },
+    mounted(){
+        console.log('view mounted');
+
+        var url = new URL(location.href);
+        this.returnId = url.searchParams.get("returnId");
+        if (!this.returnId) {
+            alert('returnId is null');
+            return;
+        }
+
+        this.getReturnById();
+    },
+    methods:{
+        handleUpdateAction(){
+            console.log('update action click');
+            this.updateReturnAction();
+        },
+        updateReturnAction(){
+            axios.post('/return/updateAction',{
+                returnId:this.returnId,
+                action:this.action
+            })
+            .then(function(response){
+                console.log(response);
+                alert('处理方式更新成功');
+                app.getReturnById();
+            })
+            .catch(function(error){
+                console.log(error);
+                
+            })
+        },
+        getReturnById(){
+            axios.get('/return/getById',{
+                params:{
+                    returnId:this.returnId
+                }
+            })
+            .then(function (response) {
+                console.log(response);
+                var aReturn = response.data;
+                app.orderId = aReturn.orderId;
+                app.orderTimestamp = aReturn.orderTimestamp;
+                app.customerId = aReturn.customerId;
+                app.customerName = aReturn.customerName;
+                app.mobile = aReturn.mobile;
+                app.email = aReturn.email;
+                app.status = aReturn.status;
+                app.action = aReturn.action;
+                app.selectedAction = aReturn.action;
+                app.productCode = aReturn.productCode;
+                app.productName = aReturn.productName;
+                app.quantity = aReturn.quantity;
+                app.reason = aReturn.reason;
+                app.opened = aReturn.opened;
+                app.comment = aReturn.comment;
+                app.createTimestamp = aReturn.createTimestamp;
+                app.updateTimestamp = aReturn.updateTimestamp;
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        }
+    }
+})
