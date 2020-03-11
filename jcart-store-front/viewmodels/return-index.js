01e3c01d6375f2e873b2f1a1f62085b14c501d2b
zmz <zmz20000502@163.com>
2020-03-11 13:38:05 +0800

    fix page

--- jcart-store-front/viewmodels/return-index.js
+++ jcart-store-front/viewmodels/return-index.js
@@ -1,34 +1,41 @@
 var app = new Vue({
     el: '#app',
     data: {
-        pageInfo:'',
-        pageNum:1
+        pageInfo: '',
+        pageNum: 1,
+        statuses: [
+            { value: 0, label: '待处理' },
+            { value: 1, label: '待取货' },
+            { value: 2, label: '正在处理' },
+            { value: 3, label: '完成' },
+            { value: 4, label: '拒绝' }
+        ]
     },
-    mounted(){
+    mounted() {
         console.log('view mounted');
-        
+
         this.searchReturn();
     },
-    methods:{
-        handlePageChange(val){
-            console.log('page change click',val);
+    methods: {
+        handlePageChange(val) {
+            console.log('page change click', val);
             this.pageNum = val;
             this.searchReturn();
         },
-        searchReturn(){
-            axios.get('/return/getList',{
-                params:{
-                    pageNum:this.pagNum
+        searchReturn() {
+            axios.get('/return/getList', {
+                params: {
+                    pageNum: this.pagNum
                 }
             })
-            .then(function(response){
-                console.log(response);
-                app.pageInfo = response.data;
-            })
-            .catch(function(error){
-                console.log(error);
-                
-            })
+                .then(function (response) {
+                    console.log(response);
+                    app.pageInfo = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+
+                })
         }
     }
 })
