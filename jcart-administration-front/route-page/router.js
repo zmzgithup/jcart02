6db3347251ef4cfa5107df29f2d8c22c0835d17b
zmz <zmz20000502@163.com>
2020-03-13 14:10:58 +0800

    fix page

--- jcart-administration-front/route-page/router.js
+++ jcart-administration-front/route-page/router.js
@@ -1,7 +1,8 @@
 
 const routes = [
     { path: '/product/search', component: ProductSearchRoutePage },
-    { path: '/customer/search', component: CustomerSearchRoutePage }
+    { path: '/customer/search', component: CustomerSearchRoutePage },
+    { path: '/order/search', component: OrderSearchRoutePage }
 ];
 
 const router = new VueRouter({
