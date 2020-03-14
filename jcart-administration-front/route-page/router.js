831ce1809902a8a54e9d1a48420d0af11cc34b80
zmz <zmz20000502@163.com>
2020-03-14 17:06:01 +0800

    fix page

--- jcart-administration-front/route-page/router.js
+++ jcart-administration-front/route-page/router.js
@@ -1,15 +1,19 @@
 
 const routes = [
+    { path: '/administrator/updateprofile', component: AdministratorUpdateProfileRoutePage },
+    { path: '/administrator/index', component: AdministratorIndexRoutePage },
+    { path: '/administrator/create', component: AdministratorCreateRoutePage },
+    { path: '/administrator/update/:administratorId', component: AdministratorUpdateRoutePage },
+
     { path: '/product/search', component: ProductSearchRoutePage },
+    { path: '/product/create', component: ProductCreateRoutePage },
     { path: '/product/update/:productId', component: ProductUpdateRoutePage },
+    
     { path: '/customer/search', component: CustomerSearchRoutePage },
     { path: '/order/search', component: OrderSearchRoutePage },
     { path: '/return/search', component: ReturnSearchRoutePage },
     
-    { path: '/administrator/updateprofile', component: AdministratorUpdateProfileRoutePage },
-    { path: '/administrator/index', component: AdministratorIndexRoutePage },
-    { path: '/administrator/create', component: AdministratorCreateRoutePage },
-    { path: '/administrator/update/:administratorId', component: AdministratorUpdateRoutePage }
+   
 ];
 
 const router = new VueRouter({
