76270ffc6af074ae10c732dacfe16eae1cdbb1a6
zmz <zmz20000502@163.com>
2020-03-22 08:48:29 +0800

    first init support service

---
+++ jcart-support-back/src/test/java/io/zmz/jcartsupportback/JcartSupportBackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zmz.jcartsupportback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcartSupportBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
